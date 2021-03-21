367d1694ed72502a5219e97e97a3e8509531433d
Juan Espares <juloespares@yahoo.com.au>
2021-03-21 19:42:26 +1000

    Aircraft package initial implementation

---
+++ test/towersim/aircraft/PassengerAircraftTest.java
@@ -0,0 +1,20 @@
+package towersim.aircraft;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PassengerAircraftTest {
+
+    //Field Variables
+
+
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void getTotalWeightTest() {
+
+    }
+}
