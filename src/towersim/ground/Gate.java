36439d906b53973d75da43264794d3b6ef47f8fe
Juan Espares <juloespares@yahoo.com.au>
2021-03-22 19:19:06 +1000

    Gate class implemented and tested

--- src/towersim/ground/Gate.java
+++ src/towersim/ground/Gate.java
@@ -1,5 +1,102 @@
 package towersim.ground;
 
+import towersim.aircraft.Aircraft;
+import towersim.util.NoSpaceException;
+
+/**
+ * Represents an aircraft gate with facilities for a single aircraft to be
+ * parked.
+ */
 public class Gate {
 
+    /** identifying number of this gate */
+    private final int gateNumber;
+
+    /** The aircraft parked at this gate */
+    private Aircraft aircraftPark;
+
+    /**
+     * Creates a new Gate with the given unique gate number.
+     * Gate numbers should be unique across all terminals in the airport.
+     *
+     * Initially, there should be no aircraft occupying the gate.
+     * @param gateNumber identifying number of this gate
+     */
+    public Gate (int gateNumber) {
+        this.gateNumber = gateNumber;
+        this.aircraftPark = null;
+    }
+
+    /**
+     * Returns this gate's gate number.
+     * @return gate number
+     */
+    public int getGateNumber() {
+        return this.gateNumber;
+    }
+
+    /**
+     * Parks the given aircraft at this gate, so that the gate becomes occupied.
+     * If the gate is already occupied, then a NoSpaceException should be
+     * thrown and the aircraft should not be parked.
+     * @param aircraft aircraft to park at gate
+     * @throws NoSpaceException if the gate is already occupied by an aircraft
+     */
+    public void parkAircraft(Aircraft aircraft) throws NoSpaceException {
+        if (this.aircraftPark != null) {
+            throw new NoSpaceException();
+        }
+        this.aircraftPark = aircraft;
+    }
+
+    /**
+     * Removes the currently parked aircraft from the gate.
+     * If no aircraft is parked at the gate, no action should be taken.
+     */
+    public void aircraftLeaves() {
+        if (this.aircraftPark != null) {
+            this.aircraftPark = null;
+        }
+    }
+
+    /**
+     * Returns true if there is an aircraft currently parked at the gate, or
+     * false otherwise.
+     * @return whether an aircraft is currently parked
+     */
+    public boolean isOccupied() {
+        return (this.aircraftPark != null);
+    }
+
+    /**
+     * Returns the aircraft currently parked at the gate, or null if there is
+     * no aircraft parked.
+     * @return currently parked aircraft
+     */
+    public Aircraft getAircraftAtGate() {
+        return this.aircraftPark;
+    }
+
+    /**
+     * Returns the human-readable string representation of this gate.
+     * The format of the string to return is
+     *
+     * Gate gateNumber [callsign]
+     * where gateNumber is the gate number of this gate and callsign is the
+     * callsign of the aircraft parked at this gate, or empty if the gate is
+     * unoccupied.
+     * For example: "Gate 15 [ABC123]" or "Gate 24 [empty]".
+     * @return string representation of this gate
+     */
+    @Override
+    public String toString() {
+        String str;
+        if (this.isOccupied()) {
+            str = String.format("Gate %d [%s]", this.getGateNumber(),
+                    this.getAircraftAtGate().getCallsign());
+        } else {
+            str = String.format("Gate %d [empty]", this.getGateNumber());
+        }
+        return str;
+    }
 }
--- test/towersim/ground/GateTest.java
+++ test/towersim/ground/GateTest.java
@@ -2,8 +2,145 @@ package towersim.ground;
 
 // add any required imports here
 
-public class GateTest {
+import org.junit.Before;
+import org.junit.Test;
+import towersim.aircraft.Aircraft;
+import towersim.aircraft.AircraftCharacteristics;
+import towersim.aircraft.FreightAircraft;
+import towersim.aircraft.PassengerAircraft;
+import towersim.tasks.Task;
+import towersim.tasks.TaskList;
+import towersim.tasks.TaskType;
+import towersim.util.NoSpaceException;
 
-    // add unit tests here
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class GateTest {
+    private Gate gate1;
+    private Gate gate785;
+
+    private Aircraft plane1;
+    private Aircraft plane2;
+
+    private Task wait;
+    private Task load20;
+    private Task load60;
+    private Task away;
+    private Task takeoff;
+
+
+    @Before
+    public void setUp(){
+        //Instantiate Gate objects
+        gate1 = new Gate(1);
+        gate785 = new Gate(785);
+
+        //Instantiate Task objects
+        wait = new Task(TaskType.WAIT);
+        load20 = new Task(TaskType.LOAD,20);
+        load60 = new Task(TaskType.LOAD, 60);
+        away = new Task(TaskType.AWAY);
+        takeoff = new Task(TaskType.TAKEOFF);
+
+        //Populate task list with tasks
+        List<Task> tasks1 = new ArrayList<>();
+        tasks1.add(wait);
+        tasks1.add(load20);
+        tasks1.add(takeoff);
+        tasks1.add(away);
+        TaskList taskList1 = new TaskList(tasks1);
+
+
+        List<Task> tasks2 = new ArrayList<>();
+        tasks2.add(wait);
+        tasks2.add(load60);
+        tasks2.add(takeoff);
+        tasks2.add(away);
+        TaskList taskList2 = new TaskList(tasks2);
+        
+        //Instantiate plane object
+        plane1 = new PassengerAircraft("Passenger123",
+                AircraftCharacteristics.AIRBUS_A320,taskList1,27000,50);
+        plane2 = new FreightAircraft("Cargo899",
+                AircraftCharacteristics.BOEING_747_8F,taskList2,222110,103000);
+    }
+
+    @Test
+    public void getGateNumberTest() {
+        assertEquals("gate number did not match",1 , gate1.getGateNumber());
+        assertEquals("gate number did not match",785 , gate785.getGateNumber());
+        try {
+            assertEquals("gate number did not match", 2, gate1.getGateNumber());
+            fail();
+        } catch (AssertionError e) {
+            //passed test
+        }
+    }
+
+    @Test
+    public void parkAircraftTest() throws NoSpaceException {
+        try {
+            gate1.parkAircraft(plane1);
+            try {
+                gate1.parkAircraft(plane2);
+                fail();
+            } catch (NoSpaceException e) {
+                //Plane 2 shouldnt be able to park
+            }
+        } catch (NoSpaceException e) {
+            //Aircraft parked at empty gate
+        }
+    }
+
+    @Test
+    public void aircraftLeavesTest() throws NoSpaceException {
+        gate1.parkAircraft(plane1);
+        gate1.aircraftLeaves();
+        assertEquals("aircraft did not leave", false, gate1.isOccupied());
+    }
+
+    @Test
+    public void noAircraftLeavesTest() {
+        gate1.aircraftLeaves();
+        assertEquals("Gate contains aircraft", false, gate1.isOccupied());
+    }
+
+    @Test
+    public void isOccupied() {
+    }
+
+    @Test
+    public void getAircraftAtGate() throws NoSpaceException {
+        gate1.parkAircraft(plane2);
+        assertTrue(gate1.getAircraftAtGate() == plane2);
+
+        gate785.parkAircraft(plane1);
+        assertTrue(gate785.getAircraftAtGate() == plane1);
+        assertFalse(gate785.getAircraftAtGate() == null);
+    }
+
+    @Test
+    public void testToStringAircraft() throws NoSpaceException {
+        String gate1String = "Gate 1 [Passenger123]";
+        gate1.parkAircraft(plane1);
+        assertEquals("String did not match", gate1String, gate1.toString());
+
+        String gate1StringWrong = "Gate  1 [ Passenger123 ]";
+        assertNotEquals("String did not match", gate1StringWrong,
+                gate1.toString());
+    }
+
+    @Test
+    public void testToStringEmpty() throws NoSpaceException {
+        String gate1String = "Gate 1 [empty]";
+        assertEquals("String did not match", gate1String, gate1.toString());
+
+        String gate1StringWrong = "Gate 1 [Passenger123]";
+        assertNotEquals("String did not match", gate1StringWrong,
+                gate1.toString());
+    }
 
 }
